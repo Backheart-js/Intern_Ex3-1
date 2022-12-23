8ec239d3c0d7386aa421a7638a9e133b6941aa5d
Backheart-js <binhnguyen29122001@gmail.com>
2022-12-23 09:38:00 +0700

    fix: Background displays following Card

--- js/app.js
+++ js/app.js
@@ -4,13 +4,43 @@ const currentCard = document.querySelector('.current--card');
 const nextCard = document.querySelector('.next--card');
 const prevCard = document.querySelector('.previous--card');
 
+const currentImg = document.querySelector('.current--image')
+const nextImg = document.querySelector('.next--image')
+const prevImg = document.querySelector('.previous--image')
+
+
 const listCard = [
     currentCard,
     nextCard,
     prevCard
 ]
 
+const listImg = [
+    currentImg,
+    nextImg,
+    prevImg
+]
+
 let dataDog = [];
+
+const renderImg = function(list, data, type) {
+    if (type === 1) {   //1 là card
+        list.forEach(function(element, index) {
+            element.innerHTML = `
+                <div class="card__image">
+                    <img src="${data[index]}" alt="" />
+                </div>
+            `
+        })
+    }
+    else {  // 2 là background
+        list.forEach(function(element, index) {
+            element.innerHTML = `
+                <img src="${data[index]}" alt="" />
+            `
+        })
+    }
+}
 
 const getData = async () => {
     try {
@@ -21,21 +51,8 @@ const getData = async () => {
         let listImage = await httpRequest.get(`https://dog.ceo/api/breed/${randomDog}/images`);
         dataDog = listImage.message;
 
-        // currentCard.innerHTML = `
-        //     <div class="card__image">
-        //         <img src="${dataDog[0]}" alt="" />
-        //     </div>
-        // `
-        listCard.forEach(function(card, index) {
-            card.innerHTML = `
-                <div class="card__image">
-                    <img src="${dataDog[index]}" alt="" />
-                </div>
-            `
-        })
-
-
-
+        renderImg(listCard, dataDog,1); //render ra Card
+        renderImg(listImg, dataDog,2);  //render ra background đằng sau
     } catch (error) {
         throw error;
     }
